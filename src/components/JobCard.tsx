fb468e9b70f20805457f5d7544dc4025374b1e00
shivaram-std <shivaram3332@gmail.com>
2025-12-19 00:36:26 +0530

    add reusable job card component

---
+++ src/components/JobCard.tsx
@@ -0,0 +1,17 @@
+import { JobApplication } from '../types/job';
+
+interface JobCardProps {
+  job: JobApplication;
+}
+
+export default function JobCard({ job }: JobCardProps) {
+  return (
+    <div className="job-card">
+      <h3>{job.company}</h3>
+      <p>{job.role}</p>
+      <span>Status: {job.status}</span>
+      <p>Applied on: {job.appliedDate}</p>
+    </div>
+  );
+}
+
